d690b91e1a46dbb38cbc7435ee1b48f388a488e1
Alok Vishwakarma <avalokkumar@gmail.com>
2023-04-28 11:53:20 +0530

    removing partial changes

--- src/main/java/com/clay/designp/facade/Reservation.java
+++
@@ -1,9 +0,0 @@
-package com.clay.designp.facade;
-
-import java.util.Date;
-
-class Reservation {
-    public void reserveRoom(Room room, Date date, int nights, String customer) {
-        // Reservation logic
-    }
-}
